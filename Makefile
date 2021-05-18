dbbcadd80813ab91e59691033644b25fe0979570
dantrag <dantrag.666@gmail.com>
2021-05-19 01:07:41 +0200

    Added command line interface

--- Makefile
+++ Makefile
@@ -3,9 +3,10 @@ TARGET_EXEC ?= springsim
 BUILD_DIR ?= ./builds/nongui
 INC_DIRS ?= ./simulator
 SRC_DIRS ?= ./simulator
+SRC_LIB_DIRS ?= ./simulator/tclap
 
 EXCLUDE_SRC_FILES := $(SRC_DIRS)/ui/%.cpp
-SRCS := $(filter-out $(EXCLUDE_SRC_FILES),$(shell find $(SRC_DIRS) -name *.cpp))
+SRCS := $(filter-out $(EXCLUDE_SRC_FILES),$(shell find $(SRC_DIRS) $(SRC_LIB_DIRS) -type f \( -iname \*.cpp -o -iname \*.c \)))
 OBJS := $(SRCS:%=$(BUILD_DIR)/%.o)
 DEPS := $(OBJS:.o=.d)
 
@@ -16,6 +17,11 @@ CPPFLAGS ?= $(INC_FLAGS) -MMD -MP -std=gnu++11
 
 $(BUILD_DIR)/$(TARGET_EXEC): $(OBJS)
 	$(CXX) $(OBJS) -o $@ $(LDFLAGS)
+
+# c source
+$(BUILD_DIR)/%.c.o: %.c
+	$(MKDIR_P) $(dir $@)
+	$(CXX) $(CPPFLAGS) $(CXXFLAGS) -c $< -o $@
 
 # c++ source
 $(BUILD_DIR)/%.cpp.o: %.cpp
--- simulator/main.cpp
+++ simulator/main.cpp
@@ -1,3 +1,7 @@
+#include <cstdio>
+#include <iostream>
+#include <fstream>
+
 #ifdef QT_CORE_LIB
 #include <QApplication>
 #include <QString>
@@ -6,6 +10,30 @@
 #endif
 
 #include "backend/SpringSimulator.h"
+
+#ifndef QT_CORE_LIB
+#include "tclap/CmdLine.h"
+
+void pngToArray(std::string filename, std::vector<std::vector<int>>& pixel_array, std::string current_path) {
+  std::string array_filename = filename + ".array";
+  std::string command = "python " + current_path + "/png_to_array.py " + filename + " " + array_filename;
+  std::system(command.c_str());
+
+  std::ifstream array_file;
+  array_file.open(array_filename);
+  int height = 0, width = 0;
+  array_file >> height >> width;
+  pixel_array.resize(height, std::vector<int>(width));
+  for (int i = 0; i < height; ++i) {
+    for (int j = 0; j < width; ++j) {
+      array_file >> pixel_array[i][j];
+    }
+  }
+  array_file.close();
+
+  std::remove(array_filename.c_str());
+}
+#endif
 
 int main(int argc, char* argv[]) {
 #ifdef QT_CORE_LIB
@@ -16,7 +44,68 @@ int main(int argc, char* argv[]) {
   window.show();
   return a.exec();
 #else
-  auto simulator = new SpringSimulator();
+  try {
+    TCLAP::CmdLine args("Simulator control script");
+    std::vector<std::string> allowed_commands = {"init", "pass", "predict"};
+    TCLAP::ValuesConstraint<std::string> commands_constraint(allowed_commands);
+    TCLAP::ValueArg<std::string> command_argument("c", "command", "init (initialize from image), pass (simulate a laser pass), or predict (find a pass to reach a desired target shape)", true, "", &commands_constraint);
+    TCLAP::MultiArg<double> params_argument("p", "params", "laser pass XY coordinates (2 numbers per point, at least 2 points)", false, "coordinates");
+    TCLAP::ValueArg<std::string> settings_argument("s", "settings", "simulator parameters file (*.cfg)", false, "", "settings file");
+    TCLAP::ValueArg<std::string> input_argument("i", "input", "*.png if initialize from bitmask, or *.xml if read from saved state", true, "", "input file");
+    TCLAP::ValueArg<std::string> target_argument("t", "target", "file with XY coordinates of shape outline (2 numbers per line)", false, "", "target file");
+    TCLAP::ValueArg<std::string> output_argument("o", "output", "will write a suggested pass (XY coordinates of points) for command ""predict"", or XML with resulting state for ""init"" and ""pass""", false, "", "output file");
+
+    args.add(command_argument);
+    args.add(params_argument);
+    args.add(settings_argument);
+    args.add(input_argument);
+    args.add(target_argument);
+    args.add(output_argument);
+    args.parse(argc, argv);
+
+    auto simulator = new SpringSimulator();
+
+    std::string settings_file = settings_argument.getValue();
+    if (!settings_file.empty()) {
+      // todo: load from settings file
+    } else {
+      std::cerr << "Warning: no settings file provided, using default (use -s)" << std::endl;
+    }
+
+    std::string command = command_argument.getValue();
+    std::string input_file = input_argument.getValue();
+
+    if (command == "init") {
+      try {
+        std::vector<std::vector<int>> pixel_array;
+        std::string current_path = argv[0];
+        current_path = current_path.substr(0, current_path.find_last_of("\\/"));
+        pngToArray(input_file, pixel_array, current_path);
+        if (pixel_array.empty() || pixel_array[0].empty()) {
+          throw std::invalid_argument("");
+        }
+        if (pixel_array[0][0] != 0) {
+          for (auto& row : pixel_array) {
+            for (auto& pixel : row) {
+              pixel = 255 - pixel;
+            }
+          }
+        }
+        simulator->initializeFromPixelArray(pixel_array, 1.0, [](int pixel) { return pixel > 0; });
+      } catch (...) {
+        std::cerr << "Error: cannot read pixel array from the input file" << std::endl;
+      }
+    } else if (command == "pass") {
+      // todo: run linear pass
+    } else if (command == "predict") {
+      // todo: run prediction
+    }
+  } catch (TCLAP::ArgException &e) {
+    std::cerr << "Error: " << e.error() << " for argument " << e.argId() << std::endl;
+  } catch (...) {
+    std::cerr << "Unknown error" << std::endl;
+  }
+
   // change settings here, if needed, e.g.:
   // simulator->settings()->setHeaterSpeed(20.0);
 
